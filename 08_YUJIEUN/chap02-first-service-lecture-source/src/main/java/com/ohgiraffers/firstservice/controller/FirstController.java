aff9c8cdf0debf9fb344526c71e7c6959b920833
jieun <yudaeun222@gmail.com>
2025-06-11 09:53:58 +0900

    add

---
+++ 08_YUJIEUN/chap02-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/controller/FirstController.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.firstservice.controller;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    private Environment environment;
+
+    public FirstController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "First Service is OK. Port = " + environment.getProperty("local.server.port");
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        return "first-request header : " + header;
+    }
+
+}
